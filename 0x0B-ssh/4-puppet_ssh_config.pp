8912b8b5d1f492c940cf2cd3a11f07df28355c06
robertrowe1013 <robertrowe1013@gmail.com>
2020-08-06 15:37:19 +0000

    task 4

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
 # puppet config
+# puppet set up
+file_line { 'NoPassword':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
+}
+file_line { 'Identify':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/holberton',
+}
