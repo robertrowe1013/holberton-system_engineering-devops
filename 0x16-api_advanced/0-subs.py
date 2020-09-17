03751e23cf7a4ec5f3619d8c2a36b00fcb340308
robertrowe1013 <robertrowe1013@gmail.com>
2020-09-17 23:54:37 +0000

    pep8 garbage

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,9 @@ import sys
 
 def number_of_subscribers(subreddit):
     """ print number of subs """
-    sub_list = requests.get('https://reddit.com/r/' + subreddit + '/about/.json', headers = {'User-agent': 'robertrowe1013'}).json().get('data')
+    url = 'https://reddit.com/r/' + subreddit + '/about/.json'
+    sub_list = requests.get(url, headers={'User-agent':
+                            'robertrowe1013'}).json().get('data')
     if sub_list is None:
         return (0)
     sl_count = sub_list.get('subscribers')
