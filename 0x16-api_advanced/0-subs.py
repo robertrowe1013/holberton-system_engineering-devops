df5d2a70690594b0a9e42adbf78011913a6b6f7d
robertrowe1013 <robertrowe1013@gmail.com>
2020-09-17 23:39:18 +0000

    task 0, no count

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" comments """
+import requests
+import sys
+
+
+def number_of_subscribers(subreddit):
+    """ print number of subs """
+    sub_list = requests.get('https://reddit.com/r/' + subreddit + '/about/.json', headers = {'User-agent': 'robertrowe1013'}).json().get('data')
+    if sub_list is None:
+        return (0)
+    return (1)
