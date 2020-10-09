0e5f58034c3bfdecdcac0bf1f939dfd85f328b19
robertrowe1013 <robertrowe1013@gmail.com>
2020-10-09 01:41:25 +0000

    try again

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,12 @@
 # fix nginx
+
 file {'/etc/default/nginx':
   ensure  => present,
   path    => '/etc/default/nginx',
   content => 'ULIMIT="-n 4096"',
 }
-exec { 'restart':
+
+-> exec { 'restart':
   provider => 'shell',
   command  => 'sudo service nginx restart',
   path     => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ]
